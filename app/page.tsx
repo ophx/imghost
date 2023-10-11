1bdd456e90d736c9c4556729489098b536724a20
ophx <castytastyhappybat@gmail.com>
2023-10-10 21:47:22 -0400

    fsdf

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,97 @@
-import Image from 'next/image'
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      {/* Navbar */}
+      <div className="fixed top-8 px-8 bg-transparent w-full flex items-center justify-between">
+        <div>
+          <Link href="/">
+            <p className="text-gray-300 text-xl">OH</p>
+          </Link>
+        </div>
+        <div></div>
+      </div>
+
+      {/* Main */}
+      <div className="hero min-h-screen">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <p className="text-white text-4xl">OphxHost</p>
+            <p className="text-gray-300 text-xl max-w-lg py-4">Private & secure image host for Discord, Revolt or any other chat app that supports embeds.</p>
+            <label htmlFor="loginModal" className="btn btn-outline border-blue-600 hover:border-blue-700 hover:bg-blue-700 text-blue-600 hover:text-white mr-4">
+              Login
+            </label>
+            <label htmlFor="registerModal" className="btn btn-outline border-blue-600 hover:border-blue-700 hover:bg-blue-700 text-blue-600 hover:text-white mr-4">
+              Register
+            </label>
+            <Link href="https://discord.gg/3DRqNct4vM" target="_blank" className="btn btn-outline border-[#5865F2] hover:border-[#5865F2] hover:bg-[#5865F2] text-[#5865F2] hover:text-white">
+              Discord
+            </Link>
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      {/* Footer */}
+      <div className="fixed bottom-8 px-8 bg-transparent w-full">
+        <p className="text-gray-300 text-sm text-center">&copy; 2023 OphxHost &mdash; All rights reserved.</p>
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      {/* Login Modal */}
+      <input type="checkbox" id="loginModal" className="modal-toggle" />
+      <div className="modal">
+        <div className="modal-box bg-gray-900 rounded-lg shadow-lg">
+          <p className="text-white text-2xl text-center">Login</p>
+          <div className="px-4 space-y-4">
+            <div>
+              <p className="text-gray-300 uppercase text-sm">Username</p>
+              <input type="text" className="input input-bordered focus:outline-none bg-transparent placeholder-gray-300 text-gray-300 w-full" />
+            </div>
+            <div>
+              <p className="text-gray-300 uppercase text-sm">Password</p>
+              <input type="password" className="input input-bordered focus:outline-none bg-transparent placeholder-gray-300 text-gray-300 w-full" />
+            </div>
+            <div>
+              <button className="btn bg-blue-700 hover:bg-blue-700 text-white w-full">
+                Submit
+              </button>
+            </div>
+          </div>
+        </div>
+        <label className="modal-backdrop" htmlFor="loginModal">Close</label>
       </div>
-    </main>
+
+      {/* Register Modal */}
+      <input type="checkbox" id="registerModal" className="modal-toggle" />
+      <div className="modal">
+        <div className="modal-box bg-gray-900 rounded-lg shadow-lg">
+          <p className="text-white text-2xl text-center">Register</p>
+          <div className="px-4 space-y-4">
+            <div>
+              <p className="text-gray-300 uppercase text-sm">Username</p>
+              <input type="text" className="input input-bordered focus:outline-none bg-transparent placeholder-gray-300 text-gray-300 w-full" />
+            </div>
+            <div>
+              <p className="text-gray-300 uppercase text-sm">Password</p>
+              <input type="password" className="input input-bordered focus:outline-none bg-transparent placeholder-gray-300 text-gray-300 w-full" />
+            </div>
+            <div>
+              <p className="text-gray-300 uppercase text-sm">Confirm Password</p>
+              <input type="password" className="input input-bordered focus:outline-none bg-transparent placeholder-gray-300 text-gray-300 w-full" />
+            </div>
+            <div>
+              <p className="text-gray-300 uppercase text-sm">Invite Code</p>
+              <input type="text" className="input input-bordered focus:outline-none bg-transparent placeholder-gray-300 text-gray-300 w-full" />
+            </div>
+            <div>
+              <button className="btn bg-blue-700 hover:bg-blue-700 text-white w-full">
+                Submit
+              </button>
+            </div>
+          </div>
+        </div>
+        <label className="modal-backdrop" htmlFor="registerModal">Close</label>
+      </div>
+    </>
   )
 }
